f5c08e461fe78da1b6ae05dfc6ddf325cf8b9ab2
Kal Ahmed <kal@brightstardb.com>
2013-06-12 11:31:33 +0100

    Fixed samples.
    Removed licensing from samples and fixed references to include all
    required assemblies.

--- samples/Samples/Embedded/SamplesConfiguration.cs
+++ samples/Samples/Embedded/SamplesConfiguration.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using BrightstarDB.Licensing;
 
 namespace BrightstarDB.Samples
 {
@@ -11,28 +10,14 @@ namespace BrightstarDB.Samples
     /// </summary>
     static class SamplesConfiguration
     {
-
-        // NOTE: These values are your private license information.
-        // You should always take steps to ensure that this information is 
-        // protected in any application you distribute - e.g. by using
-        // a .NET obfuscation tool to encrypt the string constants.
-
-        private const string LicensedUserId = ""; // TODO: Enter the licensed user ID (email address) from your license here
-        private const string LicenseKey = ""; // TODO: Enter the license key from your license here
-
-
         /// <summary>
         /// OPTIONAL: If you would like the samples to store their data in a different folder, replace
         /// the value of this property with the path to use (the directory will be created if it does not exist)
         /// </summary>
-        
         public static string StoresDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\BrightstarSamplesData";
 
         public static void Register()
         {
-            // Registers the license with BrightstarDB
-            License.Validate(LicensedUserId, LicenseKey);
-
             // Ensure that the directory we want to use for storing samples data exists.
             // If it does not, create it.
             var dir = new DirectoryInfo(StoresDirectory);
