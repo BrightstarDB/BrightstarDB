27b5a25afd8015a615d8daa9c98f8ae0e25dc777
Kal Ahmed <kal@brightstardb.com>
2013-09-21 22:54:20 +0100

    Added mono build directory and replaced custom Version task with MSBuildCommunityTasks Version task.

---
+++ mono/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CONFIGURATION=Debug
+LIB_DIR=build
+MONO_DIR=build
+CURRENT_DIR=`pwd`
+MSBuildExtensionsPath="$CURRENT_DIR"/xbuild
+
+echo $MSBuildExtensionsPath
+cd ..
+
+if [ ! -d "$LIB_DIR" ]
+then
+	mkdir "$LIB_DIR"
+fi
+
+function buildComponent {
+	xbuild /p:Configuration="$CONFIGURATION" src/"$1"/"$2"/"$2".csproj
+	cp src/"$1"/"$2"/bin/"$CONFIGURATION"/"$2".dll "$LIB_DIR"
+}
+
+buildComponent core BrightstarDB
+#buildComponent core BrightstarDB.Service
+#buildComponent core BrightstarDB.ServerRunner
+
