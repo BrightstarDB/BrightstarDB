5666fdc36ab7278e88c9ff4e801af76fcbcbfcc0
Kal Ahmed <kal@brightstardb.com>
2013-11-04 19:03:09 +0000

    Added failing test

---
+++ src/core/BrightstarDB.Tests/EntityFramework/DotNetRdfContextTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BrightstarDB.Tests.EntityFramework
+{
+    [TestFixture]
+    public class DotNetRdfContextTests
+    {
+        [Test]
+        public void TestInitializeWithStoreConfiguration()
+        {
+            var configFilePath = Path.GetFullPath(Configuration.DataLocation + "dataObjectStoreConfig.ttl");
+            var connectionString = "type=dotNetRdf;configuration=" + configFilePath + ";storeName=example;store=http://www.brightstardb.com/tests#peopleStore";
+
+            var context = new MyEntityContext(connectionString, updateGraphUri:"http://example.org/people");
+            var alice = context.FoafPersons.FirstOrDefault(p => p.Id.Equals("alice"));
+            Assert.That(alice, Is.Not.Null);
+            Assert.That(alice.Name, Is.EqualTo("Alice"));
+            Assert.That(alice.Knows, Is.Not.Null);
+        }
+    }
+}
