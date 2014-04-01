5a8fe895580666ebe6dd35efc6c6954bc65bfe19
Kal Ahmed <kal@brightstardb.com>
2014-04-01 16:51:19 +0100

    Fixes for SPARQL connection strings
    Internally now defaults the store name to "sparql" when a SPARQL
    connection string is constructed.

---
+++ src/core/BrightstarDB.Tests/EntityFramework/SparqlContextTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace BrightstarDB.Tests.EntityFramework
+{
+    [TestFixture]
+    public class SparqlContextTests
+    {
+        [Test]
+        public void TestReadOnlyConnection()
+        {
+            var context = new MyEntityContext("type=sparql;query=http://dbpedia.org/sparql");
+            Assert.IsNotNull(context);
+        }
+
+#if !PORTABLE && !WINDOWS_PHONE // SPARQL Update not supported on these platforms yet
+        [Test]
+        public void TestReadWriteConnection()
+        {
+            var doContext =
+                new MyEntityContext(
+                    "type=sparql;query=http://example.org/sparql;update=http://example.org/update");
+            Assert.IsNotNull(doContext);
+        }
+#endif
+    }
+}
