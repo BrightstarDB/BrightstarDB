47c8f4cd7ef3238e1020b0fa701304f69e18578c
Kal Ahmed <kal@brightstardb.com>
2014-04-01 16:33:31 +0100

    Added unit test for Identifier attribute on base class

--- src/core/BrightstarDB.Tests/EntityFramework/IBaseEntity.cs
+++ src/core/BrightstarDB.Tests/EntityFramework/IBaseEntity.cs
@@ -5,6 +5,9 @@ namespace BrightstarDB.Tests.EntityFramework
     [Entity]
     public interface IBaseEntity
     {
+        [Identifier("http://example.org/entities/")]
+        string Id { get; }
+
         string BaseStringValue { get; set; }
     }
 }
