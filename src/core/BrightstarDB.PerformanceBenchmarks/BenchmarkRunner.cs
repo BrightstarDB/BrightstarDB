0452572d03f6a3f22baf4e45c597426099f93dd6
Graham Moore <gramoore@outlook.com>
2014-07-01 08:40:25 +0200

    added initial data creation for taxonomy benchmark

--- src/core/BrightstarDB.PerformanceBenchmarks/BenchmarkRunner.cs
+++ src/core/BrightstarDB.PerformanceBenchmarks/BenchmarkRunner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using BrightstarDB.Client;
 
 namespace BrightstarDB.PerformanceBenchmarks
 {
--- src/core/BrightstarDB.PerformanceBenchmarks/TaxonomyDocumentBenchmark.cs
+++ src/core/BrightstarDB.PerformanceBenchmarks/TaxonomyDocumentBenchmark.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using BrightstarDB.Client;
 
@@ -17,9 +19,9 @@ namespace BrightstarDB.PerformanceBenchmarks
         {
             var start = DateTime.UtcNow;
             CreateTaxonomy();
-            CreateDocumentsInBatches(1000);
+            CreateDocumentsInBatches(10, 10000);
             var end = DateTime.UtcNow;
-            LogOperation("created-data", "created 1million items, 2 million triples", end.Subtract(start).TotalMilliseconds.ToString(), null);
+            LogOperation("created-data", "created 500,000 triples", end.Subtract(start).TotalMilliseconds.ToString(), null);
         }
 
         private void CreateTaxonomy()
@@ -41,10 +43,10 @@ namespace BrightstarDB.PerformanceBenchmarks
                 MakeTaxonomyNode("l1-" + i, "root", sb);
                 for (var j = 0; j < countLevelTwo; j++)
                 {
-                    MakeTaxonomyNode("l2-" + j, "l1" + i, sb);
+                    MakeTaxonomyNode("l1-" + i + "-l2-" + j, "l1" + i, sb);
                     for (var k = 0; k < countLevelThree; k++)
                     {
-                        MakeTaxonomyNode("l3-" + k, "l2" + j, sb);                        
+                        MakeTaxonomyNode("l1-" + i + "-l2-" + j + "-l3-" + k, "l1-" + i + "-l2-" + j, sb);                        
                     }
                 }
             }
@@ -70,7 +72,7 @@ namespace BrightstarDB.PerformanceBenchmarks
         {
             var updateData = new UpdateTransactionData();
             updateData.InsertData = data;
-            Service.ExecuteTransaction(StoreName, updateData);
+            Service.ExecuteTransaction(StoreName, updateData);            
         }
 
         // try and create a uri length and namespace that is typical in the real world.
@@ -81,9 +83,39 @@ namespace BrightstarDB.PerformanceBenchmarks
             return String.Format(ResourcePrefix, container, id);
         }
 
-        private void CreateDocumentsInBatches(int count)
-        {            
+        private void CreateDocumentsInBatches(int batchSize, int batchItemCount)
+        {
+            var docId = 0;
+            var rnd = new Random(1000000);
+            string template = "l1-{0}-l2-{1}-l3-{2}";
+            for (int i = 0; i < batchSize; i++)
+            {
+                var sb = new StringBuilder();
+                for (int j = 0; j < batchItemCount; j++)
+                {
+                    var classification = new List<String>()
+                    {
+                        string.Format(template, rnd.Next(10), rnd.Next(100), rnd.Next(100)),
+                        string.Format(template, rnd.Next(10), rnd.Next(100), rnd.Next(100)),
+                        string.Format(template, rnd.Next(10), rnd.Next(100), rnd.Next(100))
+                    };
+                    MakeDocumentNode(docId.ToString(), classification, sb);
+                    docId++;
+                }
+                InsertData(sb.ToString());
+            }
         }
+
+        private void MakeDocumentNode(string id, IEnumerable<string> classification, StringBuilder sb)
+        {
+            MakeTriple("documents", id, "schema", "a", "schema", "Document", sb);
+
+            foreach (var c in classification)
+            {
+                MakeTriple("documents", id, "schema", "classified-by", "classification", c, sb);                
+            }
+        }
+
 
         public override void RunMix()
         {            
