bbf17efaa0b955fa22fd811c0cbcb5cfe12b7250
Kal Ahmed <kal@brightstardb.com>
2014-07-12 15:42:04 +0100

    Refactored StoreTests to test both persistence types

--- src/portable/Test/BrightstarDB.Portable.Store.Tests/StoreTests.cs
+++ src/portable/Test/BrightstarDB.Portable.Store.Tests/StoreTests.cs
@@ -11,19 +11,21 @@ using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 namespace BrightstarDB.Portable.Tests
 {
     [TestClass]
-    public class StoreTests
+    public class StoreTests : StoreTestsBase
     {
         private readonly string _runId = DateTime.Now.Ticks.ToString();
         private readonly IPersistenceManager _pm = new PersistenceManager();
 
-        [TestMethod]
-        public void TestCreateStore()
+        [DataTestMethod]
+        [DataRow(PersistenceType.AppendOnly)]
+        [DataRow(PersistenceType.Rewrite)]
+        public void TestCreateStore(PersistenceType persistenceType)
         {
             var client = GetEmbeddedClient();
-            var storeName = "TestCreateStore_" + _runId;
+            var storeName = "TestCreateStore_"  + persistenceType + "_" + _runId;
             var storePath = Path.Combine(TestConfiguration.StoreLocation, storeName);
             var dataPath = Path.Combine(storePath, "data.bs");
-            client.CreateStore(storeName);
+            client.CreateStore(storeName, persistenceType);
 
 
             Assert.IsTrue(client.DoesStoreExist(storeName));
@@ -41,15 +43,17 @@ namespace BrightstarDB.Portable.Tests
             Assert.IsFalse(client.DoesStoreExist(storeName));
         }
 
-        [TestMethod]
-        public void TestRdfImportExport()
+        [DataTestMethod]
+        [DataRow(PersistenceType.AppendOnly)]
+        [DataRow(PersistenceType.Rewrite)]
+        public void TestRdfImportExport(PersistenceType persistenceType)
         {
             var client = GetEmbeddedClient();
-            var storeName = "TestRdfImportExport_" + _runId;
+            var storeName = "TestRdfImportExport_" + persistenceType + "_" + _runId;
             var importPath = Path.Combine(TestConfiguration.StoreLocation, "import");
 
             TestHelper.CopyFile("TestData\\simple.txt", importPath, "simple.txt");
-            client.CreateStore(storeName);
+            client.CreateStore(storeName, persistenceType);
 
             // RDF import
             var job = client.StartImport(storeName, "simple.txt");
@@ -63,13 +67,15 @@ namespace BrightstarDB.Portable.Tests
             Assert.IsTrue(_pm.FileExists(exportFilePath));
         }
 
-        [TestMethod]
-        public void TestExecuteTransaction()
+        [DataTestMethod]
+        [DataRow(PersistenceType.AppendOnly)]
+        [DataRow(PersistenceType.Rewrite)]
+        public void TestExecuteTransaction(PersistenceType persistenceType)
         {
             var client = GetEmbeddedClient();
-            var storeName = "TestExecuteTransaction_" + _runId;
+            var storeName = "TestExecuteTransaction_"  + persistenceType + "_" + _runId;
             
-            client.CreateStore(storeName);
+            client.CreateStore(storeName, persistenceType);
 
             // Test a simple addition of triples
             var insertData = new StringBuilder();
@@ -108,12 +114,14 @@ namespace BrightstarDB.Portable.Tests
             Assert.IsTrue(job.JobCompletedWithErrors);
         }
 
-        [TestMethod]
-        public void TestQuery()
+        [DataTestMethod]
+        [DataRow(PersistenceType.AppendOnly)]
+        [DataRow(PersistenceType.Rewrite)]
+        public void TestQuery(PersistenceType persistenceType)
         {
             var client = GetEmbeddedClient();
-            var storeName = "TestQuery_" + _runId;
-            client.CreateStore(storeName);
+            var storeName = "TestQuery_" + persistenceType + "_" + _runId;
+            client.CreateStore(storeName, persistenceType);
 
             var insertData = new StringBuilder();
             insertData.AppendLine(@"<http://example.org/people/alice> <http://xmlns.com/foaf/0.1/name> ""Alice"".");
@@ -139,22 +147,6 @@ namespace BrightstarDB.Portable.Tests
                 Assert.IsNotNull(row.GetColumnValue("n"));
             }
 
-        }
-
-
-        private void AssertJobSuccessful(IBrightstarService client,string storeName, IJobInfo job)
-        {
-            while (!(job.JobCompletedOk || job.JobCompletedWithErrors))
-            {
-                Task.Delay(3).Wait();
-                job = client.GetJobInfo(storeName, job.JobId);
-            }
-            Assert.IsTrue(job.JobCompletedOk, "Job failed with message: {0} : {1}", job.StatusMessage, job.ExceptionInfo);
-        }
-
-        private IBrightstarService GetEmbeddedClient()
-        {
-            return BrightstarService.GetClient("type=embedded;storesDirectory=" + TestConfiguration.StoreLocation);
         }
     }
 }
