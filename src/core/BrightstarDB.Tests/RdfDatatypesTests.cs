ee43ba85350c41bf1cad1a32e5a7608bc4c07d72
Kal Ahmed <kal@brightstardb.com>
2014-08-08 16:59:03 +0100

    Use round-trip format for Double and Single values
    Issue #98

---
+++ src/core/BrightstarDB.Tests/RdfDatatypesTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using BrightstarDB.Rdf;
+using NUnit.Framework;
+
+namespace BrightstarDB.Tests
+{
+    [TestFixture]
+    public class RdfDatatypesTests
+    {
+        [Test]
+        public void TestRoundtripFormatParsing()
+        {
+            var dbl = 1.123456789;
+            var literalIn = dbl.ToString("R", CultureInfo.InvariantCulture);
+            var dblOut = (double)RdfDatatypes.ParseLiteralString(literalIn, RdfDatatypes.Double, null);
+            Assert.That(dblOut, Is.EqualTo(dbl));
+        }
+
+        [Test]
+        public void TestStandardDoubleParsing()
+        {
+            var dbl = 1.123456789;
+            var literalIn = dbl.ToString(CultureInfo.InvariantCulture);
+            var dblOut = (double)RdfDatatypes.ParseLiteralString(literalIn, RdfDatatypes.Double, null);
+            Assert.That(dblOut, Is.EqualTo(dbl));
+        }
+        
+    }
+}
