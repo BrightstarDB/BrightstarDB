c278b6b0b78439963be016e6d1e0f1b864787d8d
Juan J. Chiw <jjchiw@gmail.com>
2014-09-29 18:29:49 +0200

    Change the SampleConfiguration to use Path.Combine instead of string concatenation. Added in XmlExtensions if is on mono to convert the xdocument to xmldocument via LoadString because with the reader there was an exception mono

--- src/core/BrightstarDB/Utils/XmlExtensions.cs
+++ src/core/BrightstarDB/Utils/XmlExtensions.cs
@@ -12,19 +12,35 @@ namespace BrightstarDB.Utils
     /// </summary>
     public static class XmlExtensions
     {
+        static bool IsRunningOnMono
+        {
+            get
+            {
+                return Type.GetType("Mono.Runtime") != null;
+            }
+        }
+
 #if !PORTABLE && !WINDOWS_PHONE
         /// <summary>
         /// Utility method to convert and XDocument to an XmlDocument
         /// </summary>
         /// <param name="document">The XDocument to be converted</param>
         /// <returns>The XmlDocument representation of the provided XDocument</returns>
-		public static XmlDocument AsXmlDocument(this XDocument document)
+        public static XmlDocument AsXmlDocument(this XDocument document)
         {
             var xmlDoc = new XmlDocument();
-            using (var xmlReader = document.CreateReader())
+            if (IsRunningOnMono)
             {
-                xmlDoc.Load(xmlReader);
+                xmlDoc.LoadXml(document.ToString());
             }
+            else
+            {
+                using (var xmlReader = document.CreateReader())
+                {
+                    xmlDoc.Load(xmlReader);
+                }
+            }
+
             return xmlDoc;
         }
 #endif
