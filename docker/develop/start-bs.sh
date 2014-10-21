f4d0166451613c6b51dbc12c3b89314faff27c55
Graham Moore <gramoore@outlook.com>
2014-10-21 22:16:33 +0200

    updated docker file and runner script

--- docker/develop/start-bs.sh
+++ docker/develop/start-bs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-docker run -d -t -p=80:8090 -v /brightstardata:/brightstardata bs 
+
+docker run -d -t -p=80:8090 -v /brightstardata:/brightstardata brightstardb/brightstardb-develop
+
+
+
+
 
--- docker/stable/start-bs.sh
+++ docker/stable/start-bs.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-docker run -d -t -p=80:8090 -v /brightstardata:/brightstardata bs 
+docker run -d -t -p=80:8090 -v /brightstardata:/brightstardata brightstardb/brightstardb
+
+
+
+
 
