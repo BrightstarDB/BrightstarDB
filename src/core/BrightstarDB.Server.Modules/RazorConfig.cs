cce861e754d6c8290e0476dcf698f4dfede076aa
Kal Ahmed <kal@brightstardb.com>
2015-05-17 16:39:00 +0100

    Removed app/web.config Razor configuration
    Replaced with embedded configuration in RazorConfig.cs

---
+++ src/core/BrightstarDB.Server.Modules/RazorConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy.ViewEngines.Razor;
+
+namespace BrightstarDB.Server.Modules
+{
+    public class RazorConfig : IRazorConfiguration
+    {
+        public IEnumerable<string> GetAssemblyNames()
+        {
+            yield return "BrightstarDB";
+        }
+
+        public IEnumerable<string> GetDefaultNamespaces()
+        {
+            yield return "BrightstarDB";
+            yield return "BrightstarDB.Dto";
+        }
+
+        public bool AutoIncludeModelNamespace
+        {
+            get { return true; }
+        }
+    }
+}
