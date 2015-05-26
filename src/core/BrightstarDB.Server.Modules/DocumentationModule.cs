b87b28ef6456522aad3ae4be0a3b5189df01d855
Kal Ahmed <kal@brightstardb.com>
2015-05-26 15:38:47 +0100

    Added swagger-ui for online API documentation

---
+++ src/core/BrightstarDB.Server.Modules/DocumentationModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace BrightstarDB.Server.Modules
+{
+    public class DocumentationModule : NancyModule
+    {
+        public DocumentationModule()
+        {
+            Get["/documentation"] = parameters => View["documentation.sshtml"];
+        }
+    }
+}
