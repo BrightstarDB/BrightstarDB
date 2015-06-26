2e4938ed9b7176b19d959849ccc08d3cee40537d
Kal Ahmed <kal@brightstardb.com>
2015-06-26 16:36:24 +0100

    Remove unnecessary using statements

--- src/core/BrightstarDB/EntityFramework/Query/SparqlGeneratorQueryModelVisitor.cs
+++ src/core/BrightstarDB/EntityFramework/Query/SparqlGeneratorQueryModelVisitor.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Runtime.Remoting.Contexts;
 using Remotion.Linq;
 using Remotion.Linq.Clauses.Expressions;
 using Remotion.Linq.Clauses.ResultOperators;
