d2402b90d5b803c8293af99a4e0d4cba05f5608f
Kal Ahmed <kal@brightstardb.com>
2015-06-29 16:21:42 +0100

    Upgrade CodeGen to latest Roslyn RC

--- src/core/BrightstarDB.CodeGeneration/Generator.cs
+++ src/core/BrightstarDB.CodeGeneration/Generator.cs
@@ -254,8 +254,8 @@
             Compilation compilation;
             var references = new[]
             {
-                MetadataReference.CreateFromAssembly(typeof(object).Assembly),
-                MetadataReference.CreateFromAssembly(typeof(BrightstarException).Assembly)
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(BrightstarException).Assembly.Location)
             };
 
             // we create our own compilation with which to generate the entity context
