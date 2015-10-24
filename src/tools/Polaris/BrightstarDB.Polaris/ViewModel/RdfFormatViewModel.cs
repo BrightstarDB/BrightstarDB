8cf039cd541269cb111cde7525256a92f9c0bd8e
Kal Ahmed <kal@brightstardb.com>
2015-10-24 13:23:54 +0100

    Can now choose the export file format
    Closes #219

---
+++ src/tools/Polaris/BrightstarDB.Polaris/ViewModel/RdfFormatViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BrightstarDB.Polaris.ViewModel
+{
+    public class RdfFormatViewModel
+    {
+        public RdfFormat Format { get; private set; }
+        public string DisplayName { get; private set; }
+
+        public RdfFormatViewModel(RdfFormat fmt)
+        {
+            Format = fmt;
+            DisplayName = $"{fmt.DisplayName} ({fmt.DefaultExtension})";
+        }
+    }
+}
